89f6abe2103161a8fac2458d27457c8e1d1a91a4
iyStudy <ishidayuta.recruit@gmail.com>
2024-02-13 11:38:21 +0900

    mod bugfix
    escで終了するように変更

--- main.py
+++ main.py
@@ -7,19 +7,28 @@ from threading import Thread
 # Pygame モジュールを初期化
 pygame.mixer.init()
 
-# 再生したい音声ファイルを設定（ここでは "power.mp3"）
+# 再生したい音声ファイルを設定
 sound = pygame.mixer.Sound('power.mp3')
 
+# Tkinterウィンドウをグローバル変数として扱う
+window = None
+
 def play_sound(e):
-    # キーが'esc'でない場合のみ再生
-    if e.name != 'esc':
-        sound.play()
+    global window
+    # キーが'esc'の場合、Tkinterウィンドウを閉じてプログラムを終了
+    if e.name == 'esc':
+        if window:
+            window.destroy()
+        return
+
+    sound.play()
 
 # すべてのキー入力で play_sound 関数を呼び出す
 keyboard.on_press(callback=play_sound)
 
 # 画像を表示するための関数
 def display_image():
+    global window
     window = tk.Tk()
     img = Image.open("kinnikun.jpg")
     tk_img = ImageTk.PhotoImage(img)
@@ -29,6 +38,3 @@ def display_image():
 # 'display_image'関数を別スレッドで実行
 t = Thread(target=display_image)
 t.start()
-
-# 'esc'キーが押されるまで待つ（'esc'が押されるとプログラムが終了する）
-keyboard.wait('esc')
